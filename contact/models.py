aed1d78838d83f2fc818b750dd1460191478deaa
thfmjdrip <thiagodrip@gmail.com>
2024-06-23 19:17:32 -0300

    Add Mysql

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Contact(models.Model):
+    frist_name = models.CharField(max_length=50)
+    last = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True) #blank = true nao obrigratorio
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
