4729ff118042bbed6ca14c9a690c97f96857caf4
thfmjdrip <thiagodrip@gmail.com>
2024-06-24 20:13:50 -0300

    Update admin

--- contact/admin.py
+++ contact/admin.py
@@ -7,4 +7,11 @@ from contact import models
 # Register your models here.
 @admin.register(models.Contact)
 class ContacAdmin(admin.ModelAdmin):
+    list_display = 'id','frist_name','last','phone',
+    ordering= 'id',
+    search_fields = 'id','frist_name','last',
+    list_per_page = 1
+    list_max_show_all = 2
+    list_editable = 'frist_name',
+    list_display_links = 'last',
     ...
