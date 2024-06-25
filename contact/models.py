ac654aa4b78f43573d1a716b7910437475712e9d
thfmjdrip <thiagodrip@gmail.com>
2024-06-25 18:55:41 -0300

    Owner

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     class Meta:
@@ -23,8 +24,10 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True,upload_to='pictures/%Y/%m')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL,
-                                 blank=True,null=True
-                                 )
+                                 blank=True,null=True )
+    
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL,blank=True,
+                              null=True)
 
 
     def __str__(self):
