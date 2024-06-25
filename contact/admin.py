369f1c3cc35370423a044b793c0cf59b8446f355
thfmjdrip <thiagodrip@gmail.com>
2024-06-25 17:24:02 -0300

    create and link Category

--- contact/admin.py
+++ contact/admin.py
@@ -7,7 +7,7 @@ from contact import models
 # Register your models here.
 @admin.register(models.Contact)
 class ContacAdmin(admin.ModelAdmin):
-    list_display = 'id','frist_name','last','phone','picture','show',
+    list_display = 'id','frist_name','last','phone','picture','show','category',
     ordering= 'id',
     search_fields = 'id','frist_name','last',
     list_per_page = 5
@@ -15,3 +15,8 @@ class ContacAdmin(admin.ModelAdmin):
     list_editable = 'frist_name',
     list_display_links = 'last','id',
     ...
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'id','name',
--- contact/models.py
+++ contact/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.utils import timezone
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+
 class Contact(models.Model):
     frist_name = models.CharField(max_length=50)
     last = models.CharField(max_length=50)
@@ -10,6 +17,9 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True,upload_to='pictures/%Y/%m')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL,
+                                 blank=True,null=True
+                                 )
 
 
     def __str__(self):
